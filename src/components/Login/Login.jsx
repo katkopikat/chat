bc635275320a9900885b4f853c6c10e197c521e5
Katy Rog <katkopikat@gmail.com>
2021-05-19 11:19:32 +0300

    feat: create login form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,11 @@
+import { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import VideocamIcon from '@material-ui/icons/Videocam';
+import ChatIcon from '@material-ui/icons/Chat';
+
+import './Login.css';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -13,14 +19,65 @@ const useStyles = makeStyles((theme) => ({
 
   
 const Login = () => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
   const classes = useStyles();
+
+  const startVideoChatClick = () => {
+    console.log('start video ', name, room)
+  }
+
+  const startTextChatClick = () => {
+    console.log('start video ', name, room)
+  }
+
+  const handleChangeName = (value) => {
+    setName(value);
+  }
+
+  const handleChangeRoom = (value) => {
+    setRoom(value);
+  }
 
   return (
     <div className="login__wrapper">
        <h1>Hey, how you doing?</h1>
        <form className={classes.root} noValidate autoComplete="off">
-        <TextField id="outlined-basic" label="Name" variant="outlined" />
-        <TextField id="outlined-basic" label="Room" variant="outlined" />
+          <TextField
+          id="outlined-basic"
+          label="Name"
+          variant="outlined"
+          value={name}
+          onChange={(e) => handleChangeName(e.target.value)}
+          />
+          <TextField
+          id="outlined-basic"
+          label="Room"
+          variant="outlined"
+          value={room}
+          onChange={(e) => handleChangeRoom(e.target.value)}
+          />
+          <div className="wrapper__btns">
+            <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            startIcon={<ChatIcon />}
+            onClick={startTextChatClick}
+            >
+            Text chat
+            </Button>
+
+            <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            startIcon={<VideocamIcon />}
+            onClick={startVideoChatClick}
+            >
+            Video chat
+            </Button>
+          </div>
       </form>
     </div>
   );
